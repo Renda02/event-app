9106d121fa8ecabc621f89803a5cabe98e12e04d
Renda02 <rendaniluvhengo89@gmail.com>
2021-01-02 20:24:41 +0200

    cleaned up Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,16 @@
-import { useState, useRef } from "react";
 import styled from "styled-components";
 import { AiOutlinePlus, AiOutlineHeart } from "react-icons/ai";
 import { HiOutlineTicket } from "react-icons/hi";
-import { BiSearch } from "react-icons/bi";
-import events from "../data/events.json";
 
 import HeaderOption from "./HeaderOption";
 
 function Header() {
-  const [search, setSearch] = useState("");
-  const searchRef = useRef();
-  const [suggestionList, setSuggestionList] = useState([]);
-
-  const handleChange = () => {
-    const searchTerm = searchRef.current.value;
-    const matchingEvents = events.filter((e) => {
-      if (e.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-        return true;
-      }
-      return false;
-    });
-    if (searchTerm.length > 2) {
-      setSuggestionList(matchingEvents);
-    }
-
-    console.log(matchingEvents);
-    setSearch(searchTerm);
-  };
-
   return (
     <HeaderBar>
       <HeaderLeft>
         <Heading> Evently</Heading>
       </HeaderLeft>
-      <HeaderSearch>
-        <BiSearch />
-        <Input type="text" onChange={handleChange} ref={searchRef} />
-        {suggestionList.length > 0 && (
-          <AutoComplete>
-            {suggestionList.map((suggestion) => (
-              <AutoCompleteResult
-                onClick={() => {
-                  console.log("text");
 
-                  searchRef.current.value = suggestion.title;
-                  setSearch(suggestion.title);
-                  setSuggestionList([]);
-                }}
-              >
-                {suggestion.title}
-              </AutoCompleteResult>
-            ))}
-          </AutoComplete>
-        )}
-      </HeaderSearch>
       <HeaderRight>
         <HeaderOption Icon={AiOutlinePlus} title="Add event" />
         <HeaderOption Icon={AiOutlineHeart} title="I like" />
@@ -74,7 +31,7 @@ const HeaderBar = styled.div`
   border-bottom: 0.2px solid #f4f3f5;
   top: 0;
   position: sticky;
-  
+
   background: #f2f2f2;
 `;
 
@@ -90,47 +47,6 @@ const Heading = styled.h1`
   color: #6c61f6;
 `;
 
-const HeaderSearch = styled.div`
-  display: flex;
-  padding: 18px;
-  align-items: center;
-  border-radius: 5px;
-  height: 22px;
-  color: gray;
-  background-color: #fff;
-  position: relative;
-`;
-
-const Input = styled.input`
-  outline: none;
-  background: none;
-  border: none;
-  font-size: 1rem;
-  color: #000;
-`;
-
 const HeaderRight = styled.div`
   display: flex;
-`;
-
-const AutoComplete = styled.div`
-  padding: 5px 0 5px 0px;
-  left: 0px;
-  width: 100%;
-  background: #fff;
-  top: 40px;
-  position: absolute;
-`;
-
-const AutoCompleteResult = styled.p`
-  font-size: 1rem;
-  color: #000;
-  width: 100%;
-  height: 18px;
-  padding-left: 20px;
-
-  &:hover {
-    background-color: #f2f2f2;
-    cursor: pointer;
-  }
 `;
